34951d59f27b6f943bd51a6c7caf7a005a762745
ewlin <ericwilliamlin@gmail.com>
2015-10-22 13:09:06 -0400

    added some naive versions of filter, map and reduce right for now

--- array_funcs.js
+++ array_funcs.js
@@ -10,8 +10,45 @@ function arrayReduce (array, func, initialVal) {
             current_value = cb.apply(array, [current_value, current_item]); 
         }
         current_index++; 
+        console.log(current_item); 
     }
     
     return current_value; 
 } 
 
+
+function arrayReduceRight (array, func, initialVal) {
+    var reversedArr = array.reverse(); 
+    return arrayReduce(reversedArr, func, initialVal); 
+}
+
+function arrayMap (array, iterFunc) {
+    'use strict'; 
+    var cb = iterFunc, arr_len = array.length, current_index = 0; 
+    var mappedArray = []; 
+    //var current_item; 
+    while (current_index < arr_len) {
+        let current_item = array[current_index]; 
+        mappedArray[current_index] = cb.call(array, current_item); 
+        current_index++; 
+    }
+    return mappedArray; 
+}
+        
+
+function arrayFilter (array, predicate) {
+    var cb = predicate, arr_len = array.length, current_index = 0; 
+    var filteredArray = []; 
+    var current_item; 
+    
+    while (current_index < arr_len) {
+        current_item = array[current_index]; 
+        if (cb.call(array, current_item)) {
+            filteredArray.push(current_item); 
+        }
+        current_index++; 
+    }
+    return filteredArray; 
+}
+
+    
